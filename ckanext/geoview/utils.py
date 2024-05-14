494bc6aa891317def18bc87f99667114ff6f82e4
Eric Soroos <eric@derilinx.com>
2024-05-14 14:41:11 +0100

    Change the error message and code for too big

--- ckanext/geoview/utils.py
+++ ckanext/geoview/utils.py
@@ -96,9 +96,9 @@ def proxy_service_url(req, url, unzip=False):
         cl = r.headers.get("content-length")
         if cl and int(cl) > MAX_FILE_SIZE:
             toolkit.abort(
-                409,
+                413,
                 (
-                    """Content is too large to be proxied. Allowed
+                    """Dataset is too large to be previewed. Allowed
                 file size: {allowed}, Content-Length: {actual}. Url: """
                     + url
                 ).format(allowed=MAX_FILE_SIZE, actual=cl),
@@ -130,9 +130,9 @@ def proxy_service_url(req, url, unzip=False):
 
                 if length >= MAX_FILE_SIZE:
                     toolkit.abort(
-                        409,
+                        413,
                         (
-                            """Content is too large to be proxied. Allowed
+                            """Dataset is too large to be previewed. Allowed
                     file size: {allowed}, Content-Length: {actual}. Url: """
                             + url
                         ).format(allowed=MAX_FILE_SIZE, actual=length),
