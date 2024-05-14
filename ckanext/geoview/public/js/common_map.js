edb07545bf9b97fbfd562bb4c18734d8d4722edb
Eric Soroos <eric@derilinx.com>
2024-05-14 14:44:15 +0100

    Update to use mapbox tiles API (WIP)

--- ckanext/geoview/public/js/common_map.js
+++ ckanext/geoview/public/js/common_map.js
@@ -51,6 +51,20 @@
                 accessToken: mapConfig['mapbox.access_token']
           });
 
+      } else if (mapConfig.type == 'mapbox-tiles-api') {
+          if (!mapConfig['mapbox.map_id'] || !mapConfig['mapbox.access_token']) {
+            throw '[CKAN Map Widgets] You need to provide a map ID ([account]/[handle]) and an access token when using a MapBox layer. ' +
+                  'See http://www.mapbox.com/developers/api-overview/ for details';
+          }
+
+          baseLayerUrl = (isHttps ? 'https://' : 'http://') + 'api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
+          leafletBaseLayerOptions.attribution = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
+          leafletBaseLayerOptions.tileSize = 512;
+          leafletBaseLayerOptions.maxZoom = 18;
+          leafletBaseLayerOptions.zoomOffset = -1;
+          leafletBaseLayerOptions.id = mapConfig['mapbox.map_id'];
+          leafletBaseLayerOptions.accessToken = mapConfig['mapbox.access_token'];
+
       } else if (mapConfig.type == 'custom') {
           // Custom XYZ layer
           baseLayerUrl = mapConfig['custom_url'] || mapConfig['custom.url'];
