f60beb5360352c216074cbf6f7a8e15dcb38bb15
Eric Soroos <eric@derilinx.com>
2024-10-24 15:18:09 +0000

    added multiple basemaps to geojson/leaflet

--- ckanext/geoview/public/js/common_map.js
+++ ckanext/geoview/public/js/common_map.js
@@ -35,46 +35,51 @@
                 maxZoom: 18
                 });
 
-      var baseLayer;
 
       map = new L.Map(container, leafletMapOptions);
 
-      if (mapConfig.type == 'mapbox') {
+      const parseConfig = (mapConfig) => {
+        const _baseOptions = {...leafletBaseLayerOptions};       
+        var baseLayer;
+        var baseLayerUrl;
+        
+        if (mapConfig.type == 'mapbox') {
           // MapBox base map
           if (!mapConfig['mapbox.map_id'] || !mapConfig['mapbox.access_token']) {
             throw '[CKAN Map Widgets] You need to provide a map ID ([account].[handle]) and an access token when using a MapBox layer. ' +
-                  'See http://www.mapbox.com/developers/api-overview/ for details';
+              'See http://www.mapbox.com/developers/api-overview/ for details';
           }
 
           baseLayer = L.tileLayer.provider('MapBox', {
-                id: mapConfig['mapbox.map_id'],
-                accessToken: mapConfig['mapbox.access_token']
+            id: mapConfig['mapbox.map_id'],
+            accessToken: mapConfig['mapbox.access_token']
           });
 
-      } else if (mapConfig.type == 'mapbox-tiles-api') {
+        } else if (mapConfig.type == 'mapbox-tiles-api') {
           if (!mapConfig['mapbox.map_id'] || !mapConfig['mapbox.access_token']) {
             throw '[CKAN Map Widgets] You need to provide a map ID ([account]/[handle]) and an access token when using a MapBox layer. ' +
-                  'See http://www.mapbox.com/developers/api-overview/ for details';
+              'See http://www.mapbox.com/developers/api-overview/ for details';
           }
 
           baseLayerUrl = (isHttps ? 'https://' : 'http://') + 'api.mapbox.com/styles/v1/{id}/tiles/512/{z}/{x}/{y}?access_token={accessToken}';
-          leafletBaseLayerOptions.attribution = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
-          leafletBaseLayerOptions.tileSize = 512;
-          leafletBaseLayerOptions.maxZoom = 18;
-          leafletBaseLayerOptions.zoomOffset = -1;
-          leafletBaseLayerOptions.id = mapConfig['mapbox.map_id'];
-          leafletBaseLayerOptions.accessToken = mapConfig['mapbox.access_token'];
+          _baseOptions.attribution = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
+          _baseOptions.tileSize = 512;
+          _baseOptions.maxZoom = 18;
+          _baseOptions.zoomOffset = -1;
+          _baseOptions.id = mapConfig['mapbox.map_id'];
+          _baseOptions.accessToken = mapConfig['mapbox.access_token'];
 
-      } else if (mapConfig.type == 'custom') {
+        } else if (mapConfig.type == 'custom') {
           // Custom XYZ layer
-          baseLayerUrl = mapConfig['custom_url'] || mapConfig['custom.url'];
-          if (mapConfig.subdomains) leafletBaseLayerOptions.subdomains = mapConfig.subdomains;
-          if (mapConfig.tms) leafletBaseLayerOptions.tms = mapConfig.tms;
-          leafletBaseLayerOptions.attribution = mapConfig.attribution;
+          baseLayerUrl = mapConfig['url'] || mapConfig['custom_url'] || mapConfig['custom.url'];
+          if (mapConfig.subdomains) _baseOptions.subdomains = mapConfig.subdomains;
+          if (mapConfig.tms) _baseOptions.tms = mapConfig.tms;
+          _baseOptions.attribution = mapConfig.attribution;
+          _baseOptions.title = mapConfig.title;
+          
+          baseLayer = new L.TileLayer(baseLayerUrl, _baseOptions);
 
-          baseLayer = new L.TileLayer(baseLayerUrl, leafletBaseLayerOptions);
-
-      } else if (mapConfig.type == 'wms') {
+        } else if (mapConfig.type == 'wms') {
 
           baseLayerUrl = mapConfig['wms.url'];
           wmsOptions = {}
@@ -82,40 +87,56 @@
           wmsOptions['styles'] = mapConfig['wms.styles'] || '';
           wmsOptions['format'] = mapConfig['wms.format'] || 'image/png';
           if(mapConfig['wms.srs'] || mapConfig['wms.crs']) {
-              wmsOptions['crs'] = mapConfig['wms.srs'] || mapConfig['wms.crs'];
+            wmsOptions['crs'] = mapConfig['wms.srs'] || mapConfig['wms.crs'];
           }
           wmsOptions['version'] = mapConfig['wms.version'] || '1.1.1';
 
           baseLayer = new L.TileLayer.WMS(baseLayerUrl, wmsOptions);
 
 
-      } else if (mapConfig.type) {
+        } else if (mapConfig.type) {
 
-        baseLayer = L.tileLayer.provider(mapConfig.type, mapConfig)
+          baseLayer = L.tileLayer.provider(mapConfig.type, mapConfig)
 
-      } else {
-        let c = L.Control.extend({
+        } else {
+          let c = L.Control.extend({
 
-          onAdd: (map) => {
-            let element = document.createElement("div");
-            element.className = "leaflet-control-no-provider";
-            element.innerHTML = 'No map provider set. Please check the <a href="https://docs.ckan.org/projects/ckanext-spatial/en/latest/map-widgets.html">documentation</a>';
-            return element;
-          },
-          onRemove: (map) => {}
-        })
-        map.addControl(new c({position: "bottomleft"}))
+            onAdd: (map) => {
+              let element = document.createElement("div");
+              element.className = "leaflet-control-no-provider";
+              element.innerHTML = 'No map provider set. Please check the <a href="https://docs.ckan.org/projects/ckanext-spatial/en/latest/map-widgets.html">documentation</a>';
+              return element;
+            },
+            onRemove: (map) => {}
+          })
+          map.addControl(new c({position: "bottomleft"}))
 
+        }
+        return baseLayer;
       }
 
-      if (baseLayer) {
-        let attribution = L.control.attribution({"prefix": false});
-        attribution.addTo(map)
+      let baseLayers;
+      if (Array.isArray(mapConfig)) {
+          baseLayers = mapConfig.map(e=>parseConfig(e))
+      } else {
+          baseLayers = [parseConfig(mapConfig)]
+      }
+      
+      if (baseLayers[0]) {
 
-        map.addLayer(baseLayer);
+        map.addLayer(baseLayers[0]);
 
-        if (mapConfig.attribution) {
-          attribution.addAttribution(mapConfig.attribution);
+        if (baseLayers.length > 1) {
+          // layerControl handles adding the extra layers. 
+          baseMapMap = Object.fromEntries(baseLayers.map(l => [l.options.title, l]))
+          var layerControl = L.control.layers(baseMapMap, {}).addTo(map);
+        } else {
+          // layerControl adds the attributions
+          let attribution = L.control.attribution({"prefix": false});
+          attribution.addTo(map)
+          if (baseLayers[0].attribution) {
+            attribution.addAttribution(baseLayers[0].attribution);
+          }
         }
       }
 
